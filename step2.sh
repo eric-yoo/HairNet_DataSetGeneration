e099816d9b601bfe12b207b5c1178d64d61710d6
Eric Yoo <ajoije729@gmail.com>
2020-07-11 14:49:05 +0900

    step2 script

--- step2.sh
+++ step2.sh
@@ -1,3 +1,5 @@
 mkdir roots1024
+cd Hair_generate_convdata_and_imgs
+build.sh
 cd Hair_generate_convdata_and_imgs/build
 ./Hair_generate_convdata_and_imgs ../../blend_hairs/ ../../blend_hairs_convdata/ ../../roots1024/map_roots1024.data ../../blend_hairs_imgs/ 4
