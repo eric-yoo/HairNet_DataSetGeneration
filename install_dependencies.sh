8aa54c6fbc09347d2775e8964e807dc819d3a0f2
eric-yoo <ajoije729@gmail.com>
2020-07-20 00:57:07 +0900

    update scripts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,12 +9,12 @@ sudo apt-get install libglew-dev
 sudo apt-get install libglfw3-dev libglfw3
 sudo apt-get install libglm-dev
 
-wget https://www.graphics.rwth-aachen.de/media/openmesh_static/Releases/6.3/OpenMesh-6.3.tar.gz
-tar -vzxf OpenMesh-6.3.tar.gz
-cd OpenMesh-6.3
-rm CMakeCache.txt CMakeFiles/ -rf
-mkdir build
-cd build
-cmake ..
+#wget https://www.graphics.rwth-aachen.de/media/openmesh_static/Releases/6.3/OpenMesh-6.3.tar.gz
+#tar -vzxf OpenMesh-6.3.tar.gz
+#cd OpenMesh-6.3
+#rm CMakeCache.txt CMakeFiles/ -rf
+#mkdir build
+#cd build
+#cmake ..
 
 echo "fix the OpenMesh path in Hair_generate_convdata_and_imgs/CMakeLists.txt"
--- step2.sh
+++ step2.sh
@@ -1,3 +1,4 @@
-mkdir roots1024
-cd Hair_generate_convdata_and_imgs/build
-./Hair_generate_convdata_and_imgs ../../blend_hairs/ ../../blend_hairs_convdata/ ../../roots1024/map_roots1024.data ../../blend_hairs_imgs/ 4
+cd Hair_generate_convdata_and_imgs
+./build.sh
+cd build
+./Hair_generate_convdata_and_imgs /media/eric/Extreme\ SSD/blend_hairs/ ../../blend_hairs_convdata/ ../../roots1024/map_roots1024.data ../../blend_hairs_imgs/ 2
